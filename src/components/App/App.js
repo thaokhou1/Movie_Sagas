e459465c12f1c662892873b820714350e8b2102a
Khou Thao <thao.khou@gmail.com>
2019-10-04 16:21:36 -0500

    Setted up Route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
-import MovieList from '../MovieList/MovieList';
-
+import Movie from '../Movie/Movie';
+import InfoPage from '../InfoPage/InfoPage';
+import {HashRouter as Router, Route} from 'react-router-dom';
+import { connect } from 'react-redux';
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
+      <Router>
       <div className="App">
-       <header>
-         <h1>Movie List</h1>
-         <MovieList/>
-       </header>
-       
+     
+       <Route path='/' exact component={Movie}/>
+       <Route path='/movie/:id' render={({match})=><InfoPage match={match}/>}/>
       </div>
+      </Router>
     );
   }
 }
+const mapStateToProps = reduxState => ({
+  reduxState,
+});
  
-export default App;
+export default connect(mapStateToProps)(App);
