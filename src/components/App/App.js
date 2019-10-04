f7c1032fad15a3986fdf721e519e494c340524ce
Khou Thao <thao.khou@gmail.com>
2019-10-04 17:19:45 -0500

    Created Route for edit component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movie from '../Movie/Movie';
-import InfoPage from '../InfoPage/InfoPage';
 import {HashRouter as Router, Route} from 'react-router-dom';
 import { connect } from 'react-redux';
+
+//import componenets for routes
+import InfoPage from '../InfoPage/InfoPage';
+import Edit from '../Edit/Edit';
+
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
@@ -13,6 +17,7 @@ class App extends Component {
      
        <Route path='/' exact component={Movie}/>
        <Route path='/movie/:id' render={({match})=><InfoPage match={match}/>}/>
+       <Route path='/edit/:id' render={({match})=><Edit match={match}/>}/>
       </div>
       </Router>
     );
